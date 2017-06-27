ee1da91f93d1fef199908fe73e85a5ff8346768a
Anna Werner <mail@annawerner.de>
2017-06-27 16:41:13 +0200

    integrate and stick custom logo and style navigation
    added theme support for custom logo
    logo is sticky in top left corner
    navigation bar is styled

--- header.php
+++ header.php
@@ -24,21 +24,25 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'froma2c' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
 
-			$description = get_bloginfo( 'description', 'display' );
+		<div class="site-branding">
+			<?php if ( function_exists( 'the_custom_logo' ) ) :
+				the_custom_logo();
+			else :
+				if ( is_front_page() && is_home() ) : ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php else : ?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+				endif;
+			endif;?>
+
+			<!--No description in sticky logo part
+			<?php $description = get_bloginfo( 'description', 'display' );
 			if ( $description || is_customize_preview() ) : ?>
 				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
 			<?php
-			endif; ?>
+		endif; ?> end no description-->
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation nav-bar" role="navigation">
