a18153b23766e9e59ab9e21cd5bfb700d4ab120c
Anna Werner <mail@annawerner.de>
2017-06-28 16:34:54 +0200

    styles archive loop
    up to 3 columns
    catgories above posts
    featured image integration
    post excerpt instead of full content

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -9,7 +9,6 @@
 
 ?>
 
-	 <div class="post-entry">
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<p class="post-category">
@@ -22,14 +21,7 @@
 			the_title( '<h2 class="post-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
-		if ( has_post_thumbnail() ) : ?>
-    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-        <?php the_post_thumbnail(); ?>
-    </a>
-		<?php endif;
-
 		if ( 'post' === get_post_type() ) : ?>
-		<!-- no meta
 		<div class="entry-meta">
 			<?php froma2c_posted_on(); ?>
 		</div><!-- .entry-meta -->
@@ -39,7 +31,18 @@
 
 	<div class="entry-content">
 		<?php
-			the_excerpt();
+				the_content( sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'froma2c' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				) );
 
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'froma2c' ),
@@ -47,7 +50,5 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
-
-</div> <!-- .post-entry -->
 
 </article><!-- #post-<?php the_ID(); ?> -->
